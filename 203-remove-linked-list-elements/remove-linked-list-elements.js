2aac66e63b4c497ec9ef71a60c5fc6ff1b9c5f4e
𝐑 𝐀 𝐇 𝐄 𝐄 𝐒 <114414128+rahhees@users.noreply.github.com>
2025-10-29 08:10:47 +0530

    Time: 0 ms (100.00%) | Memory: 60.6 MB (43.82%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.js
@@ -0,0 +1,14 @@
+
+var removeElements = function(head, val) {
+   let sentinal = new ListNode();
+   sentinal.next = head;
+   let prev = sentinal;
+   while(prev && prev.next){
+    if(prev.next.val===val){
+        prev.next = prev.next.next;
+    }else{
+        prev = prev.next;
+    }
+   } 
+   return sentinal.next;  
+};
