ca37e1a835bb8f087aa02dfa07ab477b11ea204e
𝐑 𝐀 𝐇 𝐄 𝐄 𝐒 <114414128+rahhees@users.noreply.github.com>
2025-11-06 10:02:46 +0530

    Time: 0 ms (100.00%) | Memory: 30.9 MB (62.02%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool IsPerfectSquare(int num) {
+        int left = 1;
+        int right = num;
+        while(left<=right){
+            int mid = left +(right-left)/2;
+            long square = (long)mid*mid;
+            if(square==num){
+                return true;
+            }else if(square<num){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+            return false;
+    }
+}
