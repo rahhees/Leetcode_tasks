76537cd6c1bbcc02d736e75510d2af33e2c57614
𝐑 𝐀 𝐇 𝐄 𝐄 𝐒 <114414128+rahhees@users.noreply.github.com>
2025-11-12 07:53:09 +0530

    Time: 0 ms (100.00%) | Memory: 56.8 MB (96.54%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
@@ -4,7 +4,7 @@
  */
 var singleNonDuplicate = function(nums) {
     for(let i = 0;i<nums.length;i+=2){
-        if( nums[i]!==nums[i+1]){
+        if(i==nums.length-1 || nums[i]!==nums[i+1]){
             return nums[i];
         }
       
