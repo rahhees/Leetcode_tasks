b8fa7981805008127caf7ffabb044818b2819624
𝐑 𝐀 𝐇 𝐄 𝐄 𝐒 <114414128+rahhees@users.noreply.github.com>
2025-11-24 17:59:03 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (65.93%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} target
+ * @param {number} n
+ * @return {string[]}
+ */
+var buildArray = function(target, n) {
+   let array = [];
+   for(let i=1;i<=n;i++){
+     array.push("Push")
+    if(!target.includes(i)){
+        array.push("Pop")
+    }
+    if(i===target[target.length-1]) break;
+   }
+    return array;
+};
