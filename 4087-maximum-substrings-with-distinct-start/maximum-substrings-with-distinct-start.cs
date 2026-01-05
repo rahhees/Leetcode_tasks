97753e1c368eb8de5f7e23acf3179c42dc20f1e4
𝐑 𝐀 𝐇 𝐄 𝐄 𝐒 <114414128+rahhees@users.noreply.github.com>
2026-01-05 21:55:06 +0530

    Time: 43 ms (7.29%) | Memory: 50.7 MB (54.97%) - LeetSync

--- 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.cs
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.cs
@@ -2,7 +2,9 @@ public class Solution {
     public int MaxDistinct(string s) {
        
         var Hash = new HashSet<char>(s);
-     
+        foreach(var str in s){
+           Hash.Add(str);
+            }
         return Hash.Count;
         }
     }
