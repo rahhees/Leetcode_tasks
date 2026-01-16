6f0a9e38d6f9864e9f3d1ada1245a95a6397840c
Mohammed Rahees vp <114414128+Raheesvp@users.noreply.github.com>
2026-01-16 10:28:39 +0530

    Time: 0 ms (100.00%) | Memory: 30.8 MB (84.19%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public bool IsPowerOfTwo(int n) {
+        if(n==1)    return true;
+        if(n<=0 || n%2!=0) return false;
+
+        return IsPowerOfTwo(n/2);
+    }
+}
