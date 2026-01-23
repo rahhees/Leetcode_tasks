02bd40789c701c6f46f4c53ec1a0839047d48c5c
Mohammed Rahees vp <114414128+Raheesvp@users.noreply.github.com>
2026-01-23 09:57:22 +0530

    Time: 479 ms (34.03%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
 
-DELETE P1
-FROM person P1, person P2
-where P1.email = P2.email AND P1.id > P2.id;
+
+delete p1 from person p1, person p2 
+ where p1.email = p2.email and p1.id>p2.id;
